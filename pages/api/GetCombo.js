a19ceae6c91e2dc4af7f173bd0cddcec53ba03f9
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-06-08 17:11:56 +0500

    Update GetCombo.js

--- pages/api/GetCombo.js
+++ pages/api/GetCombo.js
@@ -2,7 +2,7 @@
 
 export default function handler(req, res) {
   res.status(200).json({
-    combo: ["qa_team", "meme_coins", "license_japan"],
+    combo: ["p2p_trading", "hamster_kombat_merch", "defi2.0_tokens"],
     date: "08-06-24"
   });
 }
