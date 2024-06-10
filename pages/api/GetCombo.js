8b5988055575845a372cbeaa18686bef91c41565
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-06-10 17:04:46 +0500

    новое комбо

--- pages/api/GetCombo.js
+++ pages/api/GetCombo.js
@@ -2,7 +2,7 @@
 
 export default function handler(req, res) {
   res.status(200).json({
-    combo: ["x", "dao", "trading_bots"],
-    date: "09-06-24"
+    combo: ["meme_coins", "shit_coins", "influencers"],
+    date: "10-06-24"
   });
 }
