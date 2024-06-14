42d1c8915cd95bbb6a097f7e6ba334ce2b1423bb
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-06-15 00:18:42 +0500

    🔃Update GetCombo.js

--- pages/api/GetCombo.js
+++ pages/api/GetCombo.js
@@ -1,22 +1,8 @@
+// pages/api/GetCombo.js
+
 export default function handler(req, res) {
-  const validUserAgents = [
-    "Python/3.11 aiohttp/3.9.3",
-    "Python/3.10 aiohttp/3.9.3",
-    "Python/3.10 aiohttp/3.9.1",
-    "Python/3.12 aiohttp/3.9.3",
-    "Python/3.12 aiohttp/3.9.5",
-    "Python/3.11 aiohttp/3.9.5",
-    "Python/3.10 aiohttp/3.9.5"
-  ];
-
-  const userAgent = req.headers['user-agent'];
-
-  if (userAgent && (userAgent.startsWith('Python') || validUserAgents.includes(userAgent))) {
-    res.status(200).json({
-      combo: ["gamefi_tokens", "margin_trading_x30", "trading_bots"],
-      date: "14-06-24"
-    });
-  } else {
-    res.status(403).json({ message: "Ты не туда полез =)" });
-  }
+  res.status(200).json({
+    combo: ["gamefi_tokens", "margin_trading_x30", "trading_bots"],
+    date: "14-06-24"
+  });
 }
