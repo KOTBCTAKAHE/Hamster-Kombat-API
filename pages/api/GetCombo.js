fff0065a3a1156fabfaa7730ff405cd96146f310
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-06-14 17:06:28 +0500

    🆕New Daily Combo

--- pages/api/GetCombo.js
+++ pages/api/GetCombo.js
@@ -2,7 +2,7 @@
 
 export default function handler(req, res) {
   res.status(200).json({
-    combo: ["it_team", "hamster_youtube_gold_button", "product_team"],
-    date: "13-06-24"
+    combo: ["gamefi_tokens", "margin_trading_x30", "trading_bots"],
+    date: "14-06-24"
   });
 }
