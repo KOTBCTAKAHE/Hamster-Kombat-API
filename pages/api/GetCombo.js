b5568b7b3911ad597c1900c728d1d34e1103fb42
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-06-16 17:03:44 +0500

    🆕New Daily Combo!

--- pages/api/GetCombo.js
+++ pages/api/GetCombo.js
@@ -2,7 +2,7 @@
 
 export default function handler(req, res) {
   res.status(200).json({
-    combo: ["p2p_trading", "apps_center_listing", "nft_collection_launch"],
-    date: "15-06-24"
+    combo: ["prediction_markets", "derivatives", "licence_japan"],
+    date: "16-06-24"
   });
 }
