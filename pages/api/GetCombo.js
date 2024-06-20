19c7baf50fec6d5dc4a6b4d9e4b1f4550c8559b5
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-06-20 19:54:39 +0500

    🆕New Daily Combo

--- pages/api/GetCombo.js
+++ pages/api/GetCombo.js
@@ -3,6 +3,6 @@
 export default function handler(req, res) {
   res.status(200).json({
     combo: ["gamefi_tokens", "villa_for_dev_team", "p2p_trading"],
-    date: "19-06-24"
+    date: "20-06-24"
   });
 }
