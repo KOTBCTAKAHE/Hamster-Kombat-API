c6855c96da7e64014749df019e83a1175ee37831
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-06-23 18:46:13 +0500

    🆕New Daily Combo

--- pages/api/GetCombo.js
+++ pages/api/GetCombo.js
@@ -2,7 +2,7 @@
 
 export default function handler(req, res) {
   res.status(200).json({
-    combo: ["anti_money_loundering", "licence_indonesia", "top10_global"],
-    date: "22-06-24"
+    combo: ["licence_vietnam", "security_team", "prediction_markets"],
+    date: "23-06-24"
   });
 }
