3ec9443d72c95ca796d838a175f7120cfa80efa2
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-07-01 02:41:11 +0500

    🆕New Daily Combo

--- pages/api/GetCombo.js
+++ pages/api/GetCombo.js
@@ -2,7 +2,7 @@
 
 export default function handler(req, res) {
   res.status(200).json({
-    combo: ["blocking_suspicious_accounts", "youtube_25_million", "compliance_officer"],
-    date: "29-06-24"
+    combo: ["kyb", "tokenomics_expert", "tg_leaders"],
+    date: "30-06-24"
   });
 }
