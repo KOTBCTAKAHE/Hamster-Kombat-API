9cf2d644d88bdc9aef0bf493517073e30a4e5226
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-07-04 01:06:29 +0500

    🆕New Daily Combo

--- pages/api/GetCombo.js
+++ pages/api/GetCombo.js
@@ -1,7 +1,7 @@
 // pages/api/GetCombo.js
 export default function handler(req, res) {
   res.status(200).json({
-    combo: ["sports_integration", "instagram", "trading_bots"],
-    date: "02-07-24"
+    combo: ["sec_transparancy", "gamefi_tokens", "web3_game_con"],
+    date: "03-07-24"
   });
 }
