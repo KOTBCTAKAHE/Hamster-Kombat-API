941a7ac9c9890f53f9a0dec23e18d43b8495630f
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-07-04 06:44:11 +0500

    🆕New Daily Combo

--- pages/api/GetCombo.js
+++ pages/api/GetCombo.js
@@ -2,6 +2,6 @@
 export default function handler(req, res) {
   res.status(200).json({
     combo: ["sec_transparancy", "gamefi_tokens", "web3_game_con"],
-    date: "03-07-24"
+    expired: "1720094400"
   });
 }
