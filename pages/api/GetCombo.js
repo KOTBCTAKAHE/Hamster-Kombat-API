8677cd88b8423fef743cc26cb81f0cc47767edb8
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-07-05 17:08:13 +0500

    Update GetCombo.js

--- pages/api/GetCombo.js
+++ pages/api/GetCombo.js
@@ -1,7 +1,8 @@
 // pages/api/GetCombo.js
+// вчера апи не менял, извиняюсь, занят был.
 export default function handler(req, res) {
   res.status(200).json({
-    combo: ["sec_transparancy", "gamefi_tokens", "web3_game_con"],
-    date: "03-07-24"
+    combo: ["villa_for_dev_team", "youtube_25_million", "sports_integration_0607"],
+    date: "05-07-24"
   });
 }
