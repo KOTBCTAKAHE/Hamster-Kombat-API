de8d5c53871c83130a3f90a973acf0cd613e5aed
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-07-07 03:10:03 +0500

    🆕New Daily Combo

--- pages/api/GetCombo.js
+++ pages/api/GetCombo.js
@@ -1,8 +1,7 @@
 // pages/api/GetCombo.js
-// вчера апи не менял, извиняюсь, занят был.
 export default function handler(req, res) {
   res.status(200).json({
-    combo: ["villa_for_dev_team", "youtube_25_million", "sports_integration_0607"],
-    date: "05-07-24"
+    combo: ["licence_europe", "staking", "sports_integration_0607"],
+    date: "06-07-24"
   });
 }
