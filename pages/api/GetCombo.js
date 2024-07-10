8f9bf12f7f2855ac3cfc9ee67b0b5af08de5cb37
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-07-11 02:15:16 +0500

    🆕New Daily Combo

--- pages/api/GetCombo.js
+++ pages/api/GetCombo.js
@@ -2,7 +2,7 @@
 
 export default function handler(req, res) {
   res.status(200).json({
-    combo: ["dex", "tg_leaders", "sports_integration_0907"],
-    date: "09-07-24"
+    combo: ["oracle", "margin_trading_x30", "sports_integration_1007"],
+    date: "10-07-24"
   });
 }
