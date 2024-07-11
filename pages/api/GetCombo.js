ed9b4eb92af846b2af1af856f9ff37bde6a7c9a6
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-07-12 03:54:08 +0500

    🆕New Daily Combo

--- pages/api/GetCombo.js
+++ pages/api/GetCombo.js
@@ -2,7 +2,7 @@
 
 export default function handler(req, res) {
   res.status(200).json({
-    combo: ["oracle", "margin_trading_x30", "sports_integration_1007"],
-    date: "10-07-24"
+    combo: ["vesting_smartcontracts", "medium", "hamster_youtube_gold_button"],
+    date: "11-07-24"
   });
 }
