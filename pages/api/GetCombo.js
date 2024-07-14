373c93031fb1d092c78fe4f07601e9db18961b84
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-07-15 01:45:30 +0500

    Update GetCombo.js

--- pages/api/GetCombo.js
+++ pages/api/GetCombo.js
@@ -3,6 +3,6 @@
 export default function handler(req, res) {
   res.status(200).json({
     combo: ["sports_integration_1407", "youtube", "anti_money_loundering"],
-    date: "11-07-24"
+    date: "14-07-24"
   });
 }
