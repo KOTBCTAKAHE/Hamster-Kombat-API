a73b12d25c96f2c4fefde6439dbe6f8f551f1c68
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-07-15 22:47:57 +0500

    🆕New Daily Combo

--- pages/api/GetCombo.js
+++ pages/api/GetCombo.js
@@ -2,7 +2,7 @@
 
 export default function handler(req, res) {
   res.status(200).json({
-    combo: ["sports_integration_1407", "youtube", "anti_money_loundering"],
-    date: "14-07-24"
+    combo: ["call_btc_rise", "product_team", "eth_pairs"],
+    date: "15-07-24"
   });
 }
