8bebead547bb8b1e98d577a3f699a4d14b16a000
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-07-16 22:39:15 +0500

    🆕New Daily Combo

--- pages/api/GetCombo.js
+++ pages/api/GetCombo.js
@@ -2,7 +2,7 @@
 
 export default function handler(req, res) {
   res.status(200).json({
-    combo: ["call_btc_rise", "product_team", "eth_pairs"],
-    date: "15-07-24"
+    combo: ["bitcoin_pizza_day", "marketing", "adv_integration_1307"],
+    date: "16-07-24"
   });
 }
