e515e21f113919023ad26f085ccbe9481211ae8a
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-07-17 19:29:22 +0500

    🆕New Daily Combo

--- pages/api/GetCombo.js
+++ pages/api/GetCombo.js
@@ -2,7 +2,7 @@
 
 export default function handler(req, res) {
   res.status(200).json({
-    combo: ["bitcoin_pizza_day", "marketing", "adv_integration_1307"],
-    date: "16-07-24"
+    combo: ["appstore_launch", "nft_collection_launch", "tg_leaders"],
+    date: "17-07-24"
   });
 }
