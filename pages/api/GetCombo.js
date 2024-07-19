05abbb907fe8bc2134ae92e7a9974483516e6dfa
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-07-19 18:27:34 +0500

    🆕New Daily Combo

--- pages/api/GetCombo.js
+++ pages/api/GetCombo.js
@@ -2,7 +2,7 @@
 
 export default function handler(req, res) {
   res.status(200).json({
-    combo: ["telegram_50m", "top10_global", "web3_academy_launch"],
-    date: "18-07-24"
+    combo: ["google_analytics", "security_audition", "medium"],
+    date: "19-07-24"
   });
 }
