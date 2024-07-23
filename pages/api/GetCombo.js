ee55923b9f468da77a9c226c004980dff600c28a
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-07-24 02:13:05 +0500

    🆕New Daily Combo

--- pages/api/GetCombo.js
+++ pages/api/GetCombo.js
@@ -2,7 +2,7 @@
 
 export default function handler(req, res) {
   res.status(200).json({
-    combo: ["long_squeeze", "youtube", "fight_fight"],
-    date: "22-07-24"
+    combo: ["hamster_youtube_channel", "bitcoin_pizza_day", "mini_game"],
+    date: "23-07-24"
   });
 }
