a871135debb92b3300e6d842be10550b5dfbfa05
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-07-25 00:51:46 +0500

    Update GetCombo.js

--- pages/api/GetCombo.js
+++ pages/api/GetCombo.js
@@ -2,7 +2,7 @@
 
 export default function handler(req, res) {
   res.status(200).json({
-    combo: ["hamster_youtube_channel", "bitcoin_pizza_day", "mini_game"],
-    date: "23-07-24"
+    combo: ["telegram_miniapp_launch", "villa_for_dev_team", "hamster_youtube_gold_button"],
+    date: "24-07-24"
   });
 }
