270a04491bbc99fc8f7732c5d5d7ed6aefa07f90
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-08-03 23:12:03 +0500

    Update GetCombo.js

--- pages/api/GetCombo.js
+++ pages/api/GetCombo.js
@@ -2,8 +2,8 @@
 
 export default function handler(req, res) {
   const data = {
-    combo: ["compliance_officer", "antihacking_shield", "influencers"],
-    date: "02-08-24"
+    combo: ["youtube_25_million", "two_factor_authentication", "hamstergpt"],
+    date: "03-08-24"
   };
 
   res.setHeader('Content-Type', 'application/json');
