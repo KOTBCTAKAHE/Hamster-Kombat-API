b4b1b070ffbe558e5096174df6ba7876a6ce8988
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-08-08 00:19:57 +0500

    снижаю дату для проверки автоматизации скрипта

--- pages/api/GetCombo.js
+++ pages/api/GetCombo.js
@@ -3,7 +3,7 @@
 export default function handler(req, res) {
   const data = {
     combo: ["healthy_hamster", "hamster_youtube_gold_button", "partnership_program"],
-    date: "07-08-24"
+    date: "06-08-24"
   };
 
   res.setHeader('Content-Type', 'application/json');
