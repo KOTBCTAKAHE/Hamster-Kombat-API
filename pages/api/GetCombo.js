148b79becb91bfb2b90be3fb9b4846a59f51af63
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-08-09 00:49:17 +0500

    Update GetCombo.js

--- pages/api/GetCombo.js
+++ pages/api/GetCombo.js
@@ -1,42 +1,8 @@
-import mongoose from 'mongoose';
+// pages/api/GetCombo.js
 
-// Определите модель для схемы combo
-const comboSchema = new mongoose.Schema({
-  combo: [String],
-  date: String
-});
+export default function handler(req, res) {
+  const data = require('../../json/combo.json');
 
-const Combo = mongoose.models.Combo || mongoose.model('Combo', comboSchema);
-
-// Подключение к базе данных
-async function connectToDatabase() {
-  if (mongoose.connection.readyState === 0) {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-  }
-}
-
-// Обработчик API
-export default async function handler(req, res) {
-  try {
-    // Подключение к базе данных
-    await connectToDatabase();
-
-    // Извлечение данных из коллекции combo
-    const comboData = await Combo.findOne().sort({ date: -1 }); // Получаем последнюю запись
-
-    if (!comboData) {
-      return res.status(404).json({ message: 'No data found' });
-    }
-
-    // Отправка данных в виде JSON
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(comboData);
-
-  } catch (error) {
-    console.error('Error fetching data from database:', error);
-    res.status(500).json({ message: 'Internal Server Error' });
-  }
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).send(JSON.stringify(data, null, 2));
 }
