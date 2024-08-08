d46ce6cce425037b2708169e7bdc77dd8cea18ed
kyt3 <stalbutylka@gmail.com>
2024-08-08 18:54:38 +0200

    Add auto update combo functionality.

---
+++ pages/api/AutoUpdateCombo.js
@@ -0,0 +1,60 @@
+export default function handler(req, res) {
+    const jsdom = require("jsdom");
+    const { JSDOM } = jsdom;
+    const { DateTime } = require("luxon");
+    const fs = require('node:fs');
+
+    const apiData = require('../../json/combo.json');
+    const cardIds = require('../../allcardids.json');
+
+    let date = DateTime.now();
+    let apiDate = DateTime.fromFormat(apiData.date, "dd-MM-yy")
+
+    console.log(date)
+    console.log(apiDate)
+
+    if (apiDate.day != date.day) {
+        let day = date.day;
+
+        let url = `https://www.cybersport.ru/tags/games/kombo-karty-v-hamster-kombat-khomiak-na-${day}-${day + 1}-avgusta-2024-goda`;
+        fetch(url, { mode: 'no-cors'})
+            .then(response => response.text())
+            .then(html => new JSDOM(html))
+            .then(dom => {
+                let tagLiList = dom.window.document.getElementsByTagName("li");
+
+                let comboArr = new Array();
+                // Перебираем все li в #list
+                for(let i = 0; i < tagLiList.length; i++) {
+                    cardIds.upgradesForBuy.forEach(card => {
+                        if (card.name == tagLiList[i].textContent.slice(0, -1)) {
+                            comboArr.push(card.id);
+                        }
+                    });
+                }
+
+                if (comboArr.length != 3) {
+                    res.status(500).send(`Failed. Found ${comboArr.length} cards of 3`);
+                }
+
+                apiData.combo = comboArr;
+                let newComboJson = {
+                                 combo: comboArr,
+                                 date: date.toFormat("dd-MM-yy")
+                               };
+
+                fs.writeFile("./json/combo.json", JSON.stringify(newComboJson), function(err) {
+                    if (err) {
+                        console.log(err);
+                        res.status(500).send('Error: ' + err)
+                    }
+                });
+
+                res.status(200).send("Success");
+
+            })
+            .catch(err => res.status(500).send('Error: ' + err));
+    } else {
+        res.status(200).send("Already updated");
+    }
+}
--- pages/api/GetCombo.js
+++ pages/api/GetCombo.js
@@ -1,10 +1,7 @@
 // pages/api/GetCombo.js
 
 export default function handler(req, res) {
-  const data = {
-    combo: ["healthy_hamster", "hamster_youtube_gold_button", "partnership_program"],
-    date: "07-08-24"
-  };
+  const data = require('../../json/combo.json');
 
   res.setHeader('Content-Type', 'application/json');
   res.status(200).send(JSON.stringify(data, null, 2));
