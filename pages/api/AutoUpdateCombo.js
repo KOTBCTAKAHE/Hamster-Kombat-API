efec7c1bbffdcd6ed841a33a2e595bd9879599d9
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-08-13 17:23:24 +0500

    Update AutoUpdateCombo.js

--- pages/api/AutoUpdateCombo.js
+++ pages/api/AutoUpdateCombo.js
@@ -5,10 +5,8 @@ import { DateTime } from "luxon";
 export default async function handler(req, res) {
     const cardIds = require('../../allcardids.json');
 
-
     let date = DateTime.now().setZone('utc');
 
-    
     if (date.hour < 12) {
         date = date.minus({ days: 1 });
     }
@@ -19,23 +17,18 @@ export default async function handler(req, res) {
     let day = date.day;
 
     try {
-        
         const { rows } = await sql`SELECT combo, TO_CHAR(date, 'DD-MM-YY') as formatted_date FROM combo ORDER BY date DESC LIMIT 1`;
-        
-        
+
         if (rows.length === 0) {
             return res.status(500).send("No data found in the database.");
         }
 
         let apiData = rows[0];
-        
-        
         let apiDate = DateTime.fromFormat(apiData.formatted_date, "dd-MM-yy");
-
 
         if (apiDate.day != date.day) {
             let url = `https://www.cybersport.ru/tags/games/kombo-karty-v-hamster-kombat-khomiak-na-${day}-${day + 1}-${monthName}-2024-goda`;
-            
+
             const response = await fetch(url, { mode: 'no-cors' });
             const html = await response.text();
             const dom = new JSDOM(html);
@@ -43,10 +36,10 @@ export default async function handler(req, res) {
 
             let comboArr = [];
             for (let i = 0; i < tagLiList.length; i++) {
-                let cardName = tagLiList[i].textContent?.slice(0, -1);  // Защита от undefined
-                
+                let cardName = tagLiList[i].textContent?.trim();  // Удаление пробелов в начале и конце
 
-                cardName = cardName.replace(/&[^\;]*;/g, "");
+                // Заменяем все спецсимволы на пробелы
+                cardName = cardName.replace(/&[^\;]*;/g, " ").trim();
 
                 console.log("Parsed card name:", cardName);  // Debugging line
 
@@ -65,7 +58,6 @@ export default async function handler(req, res) {
                 return res.status(500).send(`Failed. Found ${comboArr.length} cards of 3`);
             }
 
-
             const newComboData = {
                 combo: comboArr,
                 date: date.toFormat("yyyy-MM-dd")
@@ -74,7 +66,6 @@ export default async function handler(req, res) {
             await sql`INSERT INTO combo (combo, date) VALUES (${newComboData.combo}, ${newComboData.date})`;
 
             res.status(200).send("Success");
-
         } else {
             res.status(200).send("Already updated");
         }
