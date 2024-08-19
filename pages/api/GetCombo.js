d15123466603115a0ce6ef31d2f9bd7bab9496af
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-08-19 14:10:22 +0500

    Update GetCombo.js

--- pages/api/GetCombo.js
+++ pages/api/GetCombo.js
@@ -1,10 +1,7 @@
 // pages/api/GetCombo.js
 
 export default function handler(req, res) {
-  const data = {
-    combo: ["anti_money_loundering", "trading_bots", "compliance_officer"],
-    date: "18-08-24"
-  };
+  const data = require('../../json/combo.json');
 
   res.setHeader('Content-Type', 'application/json');
   res.status(200).send(JSON.stringify(data, null, 2));
