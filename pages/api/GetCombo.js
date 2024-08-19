5fa38bae4e08f225876cd84405402e96d1d369db
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-08-19 12:50:16 +0500

    fix 500 error
    Sorry for the inconvenience, the database query limit is reached.

--- pages/api/GetCombo.js
+++ pages/api/GetCombo.js
@@ -1,27 +1,11 @@
-import { sql } from "@vercel/postgres";
+// pages/api/GetCombo.js
 
-export default async function handler(req, res) {
-    try {
-        // Выполняем запрос к базе данных для получения последней записи
-        const { rows } = await sql`SELECT combo, TO_CHAR(date, 'DD-MM-YY') as formatted_date FROM combo ORDER BY date DESC LIMIT 1`;
+export default function handler(req, res) {
+  const data = {
+    combo: ["anti_money_loundering", "trading_bots", "compliance_officer"],
+    date: "18-08-24"
+  };
 
-        // Проверяем, нашлись ли данные в базе данных
-        if (rows.length === 0) {
-            return res.status(500).send("No data found in the database.");
-        }
-
-        // Формируем ответ в нужном формате
-        const data = {
-            combo: rows[0].combo,
-            date: rows[0].formatted_date
-        };
-
-        // Отправляем данные в формате JSON
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).send(JSON.stringify(data, null, 2));
-
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).send('Error: ' + error.message);
-    }
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).send(JSON.stringify(data, null, 2));
 }
