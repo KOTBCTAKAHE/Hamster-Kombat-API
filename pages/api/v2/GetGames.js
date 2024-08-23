31cbc90ae9725273464ffb06e357e1176caf2229
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-08-23 14:30:50 +0500

    add new games

--- pages/api/v2/GetGames.js
+++ pages/api/v2/GetGames.js
@@ -41,6 +41,18 @@ export default function handler(req, res) {
             'promoId': '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
             'interval': 30,
             'eventCount': 16,
+        },
+        'TRIM': {
+            'appToken': 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
+            'promoId': 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
+            'interval': 120,
+            'eventCount': 8,
+        },
+        'RACE': {
+            'appToken': '8814a785-97fb-4177-9193-ca4180ff9da8',
+            'promoId': '8814a785-97fb-4177-9193-ca4180ff9da8',
+            'interval': 120,
+            'eventCount': 6,
         }
     };
 
