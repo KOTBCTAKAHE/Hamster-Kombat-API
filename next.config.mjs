97ce937366ef94427de9029f3b3736e8ba7e26e4
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-09-19 17:06:40 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // Добавляем статический экспорт
+  output: 'standalone',  // Добавляем статический экспорт
 };
 
 export default nextConfig;
