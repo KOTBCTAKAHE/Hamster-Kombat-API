132b2cb4d316b72128a377e16fe28a46ef8a5f6e
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-09-23 20:31:49 +0500

    Update fetchData.js

--- fetchData.js
+++ fetchData.js
@@ -1,50 +1,45 @@
-require('dotenv').config();
-const fs = require('fs');
-const path = require('path');
-const cloudflareScraper = require('cloudflare-scraper'); // Возвращаем require
+import fs from 'fs';
+import path from 'path';
+import cloudflareScraper from 'cloudflare-scraper'; // Используем import
+
+// Получаем токен из переменной окружения
+const authorizationToken = process.env.AUTHORIZATION_TOKEN || process.env.TOKEN;
+
+if (!authorizationToken) {
+  console.error('Authorization token is not defined in environment variables');
+  process.exit(1);
+}
+
+const options = {
+  uri: 'https://api.hamsterkombatgame.io/interlude/upgrades-for-buy',
+  method: 'POST',
+  headers: {
+    'authority': 'api.hamsterkombatgame.io',
+    'Accept': '*/*',
+    'Accept-Encoding': 'application/json',
+    'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Authorization': authorizationToken,
+    'Content-Length': '0',
+    'Origin': 'https://hamsterkombatgame.io',
+    'Referer': 'https://hamsterkombatgame.io/',
+    'Sec-Ch-Ua': '"Not.A.Brand";v="99", "Chromium";v="124"',
+    'Sec-Ch-Ua-Mobile': '?1',
+    'Sec-Ch-Ua-Platform': '"Android"',
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
+  }
+};
 
 async function main() {
-  // Получаем токен из переменной окружения
-  const authorizationToken = process.env.AUTHORIZATION_TOKEN || process.env.TOKEN;
-
-  if (!authorizationToken) {
-    console.error('Authorization token is not defined in environment variables');
-    process.exit(1);
-  }
-
-  // Определяем заголовки запроса
-  const options = {
-    uri: 'https://api.hamsterkombatgame.io/interlude/upgrades-for-buy',
-    method: 'POST',
-    headers: {
-      'authority': 'api.hamsterkombatgame.io',
-      'Accept': '*/*',
-      'Accept-Encoding': 'application/json',
-      'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-      'Authorization': authorizationToken,
-      'Content-Length': '0',
-      'Origin': 'https://hamsterkombatgame.io',
-      'Referer': 'https://hamsterkombatgame.io/',
-      'Sec-Ch-Ua': '"Not.A.Brand";v="99", "Chromium";v="124"',
-      'Sec-Ch-Ua-Mobile': '?1',
-      'Sec-Ch-Ua-Platform': '"Android"',
-      'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
-    }
-  };
-
   try {
     // Делаем запрос с использованием cloudflare-scraper
     const response = await cloudflareScraper.request(options);
 
-    // Путь к json-файлу
-    const filePath = path.join(__dirname, 'json', 'all_card_ids.json');
+    const filePath = path.join(path.resolve(), 'json', 'all_card_ids.json');
 
-    // Проверяем, существует ли директория
     if (!fs.existsSync(path.dirname(filePath))) {
       fs.mkdirSync(path.dirname(filePath), { recursive: true });
     }
 
-    // Записываем данные в файл
     fs.writeFileSync(filePath, response, 'utf-8');
     console.log('Data saved to json/all_card_ids.json');
   } catch (error) {
@@ -52,5 +47,4 @@ async function main() {
   }
 }
 
-// Запуск основного процесса
 main();
