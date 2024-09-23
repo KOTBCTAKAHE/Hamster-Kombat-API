21601c419f0b1b813b3b0ba25300aa12a9c3fde1
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-09-23 20:12:19 +0500

    🆕New API route

---
+++ pages/Cards.js
@@ -0,0 +1,13 @@
+export default function handler(req, res) {
+  // Импортируем данные из JSON-файла
+  const data = require('../../json/Promo-Apps.json');
+
+  // Устанавливаем заголовок для JSON
+  res.setHeader('Content-Type', 'application/json');
+
+  // Разрешаем CORS для любых сайтов
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Возвращаем данные с отступами
+  res.status(200).send(JSON.stringify(data, null, 2)); // Отступ в 2 пробела
+}
